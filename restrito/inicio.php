f1cd01c888bd749f63a6af4fc859c8f4a4f2aa7e
Jourdan Rodrigues <thiagojourdan@gmail.com>
2016-11-17 23:22:58 -0300

    Improvement in file structure.

--- restrito/inicio.php
+++ restrito/inicio.php
@@ -1,86 +1,66 @@
 <?php 
 session_start();
 
-if(!isset($_SESSION[md5('func')])){
-   
+if (!isset($_SESSION[md5('func')])) {
     header("location: http://localhost/projeto_airfare/index.php");
-
-  }else{
-
+} else {
     $func = $_SESSION[md5('func')];
 }
-
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>Área Restrita - Airfare</title>
         <link rel="stylesheet" type="text/css" href="../view/assets/bootstrap/css/bootstrap.css">
-        
         <script src="../view/assets/bootstrap/js/jquery.js"></script>
         <script src="../view/assets/bootstrap/js/bootstrap.js"></script>
-
     </head>
     <body>
         <div class="container">
             <header class="row navbar navbar-default cabecalho"><!-- cabecalho -->
                 <div class="col-lg-8 ">
                     <div class="navbar-header">
-                        <a class="navbar-brand" href="inicio.php"><strong>AirFare - Área Adminstrativa</strong></a>
+                        <a class="navbar-brand" href="inicio.php">
+                            <strong>AirFare - Área Adminstrativa</strong>
+                        </a>
                     </div>
-
                     <ul class="nav navbar-nav navbar-default">
-                         <li class="dropdown" style="background-color:#98FB98">
+                        <li class="dropdown" style="background-color:#98FB98">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Buscar <span class="caret"></span></a>
-                           <ul class="dropdown-menu">
-                              <li><a href="#">Clientes</a></li>
-                              <li><a href="listagem_voos.php">Vôos</a></li>
-                              <li><a href="#">Pedidos</a></li>
-                              
-                              
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Clientes</a></li>
+                                <li><a href="listagem_voos.php">Vôos</a></li>
+                                <li><a href="#">Pedidos</a></li>
                             </ul>
-                          </li>
-                        
-                         <li class="dropdown" style="background-color:#98FB98">
+                        </li>
+                        <li class="dropdown" style="background-color:#98FB98">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Listar <span class="caret"></span></a>
-                           <ul class="dropdown-menu">
-                              <li><a href="#">Clientes</a></li>
-                              <li><a href="listagem_voos.php">Vôos</a></li>
-                              <li><a href="#">Pedidos</a></li>
-                              
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Clientes</a></li>
+                                <li><a href="listagem_voos.php">Vôos</a></li>
+                                <li><a href="#">Pedidos</a></li>
                             </ul>
-                          </li>
-
-                          <li class="dropdown" style="background-color:#98FB98">
+                        </li>
+                        <li class="dropdown" style="background-color:#98FB98">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Opções <span class="caret"></span></a>
-                           <ul class="dropdown-menu">
-                              <li><a href="../controller/logout_func.php">Sair do Sistema</a></li>
-                              <li role="separator" class="divider"></li>
-                              <li><a href="#">Separated link</a></li>
+                            <ul class="dropdown-menu">
+                                <li><a href="../controller/logout_func.php">Sair do Sistema</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">Separated link</a></li>
                             </ul>
-                          </li>
-
-
-                          
-                        </ul>
-
+                        </li>
+                    </ul>
                 </div>
                 <div class="col-lg-4">
                     <div class="navbar-header navbar-right">
                         <p class="navbar-text"> Bem vindo(a), <strong><?php foreach($func as $key){echo $key["nome"];} ?></strong></p>
-                        
                     </div>
                 </div>
             </header>   <!-- fim cabecalho -->
             <section class="row">
-
-                
-               <div class="col-lg-2"></div>
+                <div class="col-lg-2"></div>
                 <div class="col-lg-8" >
-
-               
-
-                   <div class="panel panel-success" >
+                    <div class="panel panel-success" >
                         <div class="panel-heading" style="background-color:lightgreen;">
                             <h3 class="panel-title text-center">
                                 <span class="glyphicon glyphicon-th">
@@ -89,11 +69,9 @@ if(!isset($_SESSION[md5('func')])){
                             </h3>
                         </div>
                     </div>
-                    <div class="panel-body">
-                       BEM VINDO
-                    </div>
+                    <div class="panel-body">BEM VINDO</div>
                 </div>
-              <div class="col-lg-2"></div> 
+                <div class="col-lg-2"></div>
             </section>
         </div>
     </body>
--- restrito/restrito.php
+++ restrito/restrito.php
@@ -1,23 +1,14 @@
 <?php
-   
- session_start(); 
+session_start();
 
-if(isset($_SESSION[md5("user")])){
-
-   header("location: http://localhost/projeto_airfare/index.php");
-
+if (isset($_SESSION[md5("user")])) {
+    header("location: http://localhost/projeto_airfare/index.php");
 }
 
-
-if(isset($_SESSION[md5("func")])){
-
+if (isset($_SESSION[md5("func")])) {
     header("location: http://localhost/projeto_airfare/restrito/inicio.php");
-
 }
-
-
 ?>
-
 <!DOCTYPE html>
 <html>
     <head>
@@ -36,7 +27,6 @@ if(isset($_SESSION[md5("func")])){
                 <div class="col-lg-4"></div>
             </header>   <!-- fim cabecalho -->
             <section class="row">
-               
                 <div class="col-lg-4"></div>
                 <div class="col-lg-4" >
                     <div class="panel panel-success" >
@@ -49,37 +39,26 @@ if(isset($_SESSION[md5("func")])){
                         </div>
                     </div>
                     <div class="panel-body">
-
                         <?php if(isset($_GET["invalid_login"])){ ?>
-                            
+
                             <div class="alert alert-danger text-center" >
                                 <strong> E-mail ou senha inválidos</strong>
                             </div>
-
                         <?php } ?>
-
-                         <?php if(isset($_GET['logout'])){ ?>
-
-                           <div class="alert alert-success text-center" >
-                             <strong>Você foi desconectado do sistema com sucesso</strong>
-                           </div>
-
-                         <?php } ?>
-
-                       
-
+                        <?php if(isset($_GET['logout'])){ ?>
+                            <div class="alert alert-success text-center" >
+                                <strong>Você foi desconectado do sistema com sucesso</strong>
+                            </div>
+                        <?php } ?>
                         <form method="POST" action="../controller/logar_func.php" >
                             <input type="email" placeholder="E-mail"  name="email" class="form-control"  required /><br>
                             <input type="password" placeholder="Senha" name="password" class="form-control" required /><br>
-
-                            
                             <button class="btn btn-primary form-control">Entrar</button>
                         </form>
                         <br>
-                       
                     </div>
                 </div>
-              <div class="col-lg-4"></div> 
+                <div class="col-lg-4"></div>
             </section>
         </div>
     </body>
